4224be935d8e46006d73d74a1612e703aeecb843
Manas <manasmahajan2003@yahoo.com>
2024-02-20 16:53:40 +0530

    Image upload

--- routes/listing.js
+++ routes/listing.js
@@ -3,16 +3,16 @@ const router= express.Router();
 const wrapAsync=require("../utils/wrapAsync.js");
 const Listing=require("../models/listing.js");
 const {isLoggedIn, isOwner, validateListing}=require("../middleware.js");
-
-
 const listingController=require("../controllers/listings.js");
+const multer  = require('multer')
+const {storage}=require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router
     .route("/")
     .get(wrapAsync(listingController.index))
-    .post(isLoggedIn,validateListing,wrapAsync(listingController.createListing));
-
-
+    .post(isLoggedIn,upload.single("listing[image]"),validateListing,wrapAsync(listingController.createListing));
+    
 
 //New route
 router.get("/new",isLoggedIn,listingController.renderNewForm);
@@ -20,7 +20,7 @@ router.get("/new",isLoggedIn,listingController.renderNewForm);
 router
     .route("/:id")
     .get(wrapAsync(listingController.showListing))
-    .put(isLoggedIn,validateListing,wrapAsync(listingController.updateListing))
+    .put(isLoggedIn,upload.single("listing[image]"),validateListing,wrapAsync(listingController.updateListing))
     .delete(isLoggedIn,isOwner,wrapAsync(listingController.deleteListing));
 
 
